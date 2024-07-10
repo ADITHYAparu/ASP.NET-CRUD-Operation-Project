abb61b34acaad8350882c78e96605cb6018846ff
Adithyacnair <adithyaapps143@gmail.com>
2024-07-10 18:06:54 +0530

    TaskManager

---
+++ Models/TaskDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models;
+
+public partial class TaskDbContext : DbContext
+{
+    public TaskDbContext()
+    {
+    }
+
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TaskTable> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if(!optionsBuilder.IsConfigured)
+        {
+        //    #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        //=> optionsBuilder.UseSqlServer("Server=LAPTOP-J6N44P2V;Database=TaskDB;Trusted_Connection=True;Encrypt=False;");
+        }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskTable>(entity =>
+        {
+            entity.ToTable("TaskTable");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/TaskTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TaskManager.Models;
+
+public partial class TaskTable
+{
+    public int Id { get; set; }
+    [DisplayName("Name")]
+    public string Name { get; set; } = null!;
+    [DisplayName("Description")]
+    public string Description { get; set; } = null!;
+}
